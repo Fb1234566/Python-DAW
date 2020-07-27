6ddfc745352fb2ee12c7838b1c47daa2b17f76d0
Filippo Benedetti <filippo.benedetti3@gmail.com>
2020-07-27 15:21:10 +0200

    Update playlist.py

--- scripts/playlist.py
+++ scripts/playlist.py
@@ -1,10 +1,17 @@
 import tkinter as tk
 
-def create_playlist(win,tracks=[]):
-    canvas=tk.Canvas(win, height=500, width=500)
-    def cerate_track():
-        label = tk.Label()
-    canvas.pack()
+def create_playlist(win,tracks=[1,2,3,4,5,6,7,8,9]):
+    playlist=tk.Canvas(win,bg='grey')
+    def cerate_track(playlist, number):
+        track=tk.Canvas(playlist,bg='white')
+        print(number)
+        track.place(relheight=0.125, relwidth=0.99,relx=0.005, rely=0.0085+(0.0085+0.125)*number)
+    for i in range(len(tracks)):
+        cerate_track(playlist, i)
 
-create_playlist(tk.Tk())
+    
+    playlist.place(relheight=0.75, relwidth=0.75, relx=0.25, rely=0.25)
+win=tk.Tk()
+win.attributes('-fullscreen', True)
+create_playlist(win)
 tk.mainloop()
