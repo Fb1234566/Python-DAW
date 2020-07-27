bea4479364b0be7f20e444de3155a1db62e60b76
Filippo Benedetti <filippo.benedetti3@gmail.com>
2020-07-27 12:31:00 +0200

    first

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import tkinter as tk
 import numpy as np
+
+#creating interface
+def create_interface():
+    main_win=tk.Tk()
+    play=tk.Button(main_win, text='play')
+    play.pack()
+    main_win.mainloop()
+
+create_interface()
