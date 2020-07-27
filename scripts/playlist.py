b833c83c65eb83a65dc2495b33e6edf6ffdb5968
Filippo Benedetti <filippo.benedetti3@gmail.com>
2020-07-27 13:03:54 +0200

    Update playlist.py

--- scripts/playlist.py
+++ scripts/playlist.py
@@ -3,7 +3,7 @@ import tkinter as tk
 def create_playlist(win,tracks=[]):
     canvas=tk.Canvas(win, height=500, width=500)
     def cerate_track():
-        label
+        label = tk.Label()
     canvas.pack()
 
 create_playlist(tk.Tk())
