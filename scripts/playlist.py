83e993036962e505ce542b3f64e07e43b438de2d
Fb1234566 <58706918+Fb1234566@users.noreply.github.com>
2020-07-27 13:01:40 +0200

    Add files via upload

---
+++ scripts/playlist.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def create_playlist(win,tracks=[]):
+    canvas=tk.Canvas(win, height=500, width=500)
+    def cerate_track():
+        label
+    canvas.pack()
+
+create_playlist(tk.Tk())
+tk.mainloop()
